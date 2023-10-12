5165f238b0952897a83ad88a7c2efb408e4dedec
Ducksel <98615937+D-u-c-k-s-e-l@users.noreply.github.com>
2023-10-12 21:45:07 +0000

    Update votd.fish
    add API variable, silence rm

--- votd.fish
+++ votd.fish
@@ -3,12 +3,14 @@
 # FILE: votd (fish file)
 # DESCRIPTION: fish script for Verse Of The Day
 
+set Votd_API 'https://www.kingjamesbibleonline.org/popular-bible-verses-widget.php'
+
 pushd (systemd-path temporary)
-rm votd > /dev/null
-curl -s 'https://www.kingjamesbibleonline.org/popular-bible-verses-widget.php' | sed -e 's/<[^>]*>//g' | sed -e 's/&quot;/"/g' | sed 's/ -/\n-/g' >> votd
-rm votd.txt > /dev/null
+rm -f votd > /dev/null
+curl -s "$Votd_API" | sed -e 's/<[^>]*>//g' | sed -e 's/&quot;/"/g' | sed 's/ -/\n-/g' >> votd
+rm -f votd.txt > /dev/null
 fmt -t votd >> votd.txt
-rm votd_color_file > /dev/null
+rm -f votd_color_file > /dev/null
 echo "[96m" >> votd_color_file
 # clear
 cat votd_color_file votd.txt
